cfeb8c7570346f0cd89f6abee5a5fcab5b253470
serplesniowy <132208333+serplesniowy@users.noreply.github.com>
2025-04-10 20:40:33 +0200

    hw6 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import ContactForm from './components/ContactForm'
-import ContactList from './components/ContactList'
-import SearchBox from './components/SearchBox'
-import { nanoid } from 'nanoid'
-import styles from './App.module.css'
+import React, { useState, useEffect } from "react";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+import SearchBox from "./components/SearchBox";
+import { nanoid } from "nanoid";
+import { useSelector, useDispatch } from "react-redux";
+import { addContact, deleteContact } from "./redux/contactsSlice";
+import { setFilter } from "./redux/filterSlice";
+import styles from "./App.module.css";
 
 const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-]
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
 const App = () => {
-  const [contacts, setContacts] = useState([])
-  const [filter, setFilter] = useState('')
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
+  const dispatch = useDispatch();
 
+  const handleAddContact = (name, number) => {
+    dispatch(addContact({ name, number }));
+  };
 
-  useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem('contacts'))
-    if (savedContacts && savedContacts.length > 0) {
-      setContacts(savedContacts)
-    } else { 
-      setContacts(initialContacts)
-    }
-  }, [])
-
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts))
-  }, [contacts])
-
-  const addContact = (name, number) => {
-    const newContact = { id: nanoid(), name, number }
-    setContacts(prevContacts => {
-      const updatedContacts = [...prevContacts, newContact]
-      localStorage.setItem('contacts', JSON.stringify(updatedContacts))
-      return updatedContacts
-    })
-  }
-
-  const deleteContact = (id) => {
-    setContacts(prevContacts => {
-      const updatedContacts = prevContacts.filter(contact => contact.id !== id)
-      localStorage.setItem('contacts', JSON.stringify(updatedContacts))
-      return updatedContacts
-    })
-  }
+  const handleDeleteContact = (id) => {
+    dispatch(deleteContact(id));
+  };
 
   const handleFilterChange = (e) => {
-    setFilter(e.target.value)
-  }
+    dispatch(setFilter(e.target.value));
+  };
 
-  const filteredContacts = contacts.filter(contact =>
+  const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
-  )
+  );
 
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>Phonebook</h1>
-      <ContactForm onAddContact={addContact} />
+      <ContactForm onAddContact={handleAddContact} />
       <SearchBox value={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
